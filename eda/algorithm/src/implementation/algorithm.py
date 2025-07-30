9a79b3d41c5d52036a510e689d6437b17810b4c4
spin3l <dev.christianlopezgarcia@gmail.com>
2025-07-30 11:59:52 +0200

    fix: working algorithm + using uv and new oceanprotocol-job-details version

--- eda/algorithm/src/implementation/algorithm.py
+++ eda/algorithm/src/implementation/algorithm.py
@@ -1,8 +1,8 @@
 from logging import getLogger
 from pathlib import Path
-from typing import Any, Optional
+from typing import Optional, Self
 
-from oceanprotocol_job_details.dataclasses.job_details import JobDetails
+from oceanprotocol_job_details.job_details import JobDetails
 from pandas import read_csv
 from ydata_profiling import ProfileReport
 
@@ -12,28 +12,28 @@ logger = getLogger(__name__)
 class Algorithm:
     def __init__(self, job_details: JobDetails):
         self._job_details = job_details
-        self.results: Optional[Any] = None
+        self.results: Optional[ProfileReport] = None
 
-    def _validate_input(self) -> "Algorithm":
-        if not self._job_details.dids or len(self._job_details.dids) == 0:
-            logger.warning("No DIDs found")
-            raise ValueError("No DIDs found")
+    def _validate_input(self) -> Self:
+        assert self._job_details.files, "No files found"
 
-        if not self._job_details.files:
-            logger.warning("No files found")
-            raise ValueError("No files found")
+    def run(self) -> Self:
+        """The algorithm entrypoint. This method does the following:
 
-    def run(self) -> "Algorithm":
+        1. Loads the input data from the first given file.
+        1. Generate the summary report.
 
+        """
+
+        # Validates the given JobDetails instance
         self._validate_input()
 
-        first_did = self._job_details.dids[0]
-        filename = self._job_details.files[first_did][0]
-
-        df = read_csv(filename, sep=None, engine="python")
+        # Get input filepath
+        input_path = self._job_details.files[0].input_files[0]
+        df = read_csv(input_path, sep=None, engine="python")
 
         self.results = ProfileReport(df, title="Profiling Report", sensitive=False)
-        logger.info(f"Generated profiling report for {filename}")
+        logger.info(f"Generated profiling report for {input_path.name}")
 
         return self
 
--- eda/algorithm/src/main.py
+++ eda/algorithm/src/main.py
@@ -1,9 +1,22 @@
-import json
-import logging
-from dataclasses import asdict
+# =========
+# Append the path of the algorithm to the sys.path
+#
+# This is needed because THIS .py file is downloaded from the given URL, whilst the rest of the implementation is not.
+# The rest of the implementation, can (and should) be added in two ways:
+# 1. ADD/COPY the implementation source code in the Dockerfile provided later to the dataspace.
+# 2. Mounted as a volume for quick development iterations.
+#
+# This step is not needed if this file contains the whole implementation of your algorithm, in which case
+# you could use the `python-monolith` version.
+import sys
+from pathlib import Path
 
-from oceanprotocol_job_details.dataclasses.constants import Paths
-from oceanprotocol_job_details.dataclasses.job_details import JobDetails
+sys.path.append("/algorithm/src")
+# ======
+
+import logging
+
+from oceanprotocol_job_details.config import config
 from oceanprotocol_job_details.job_details import OceanProtocolJobDetails
 
 from implementation.algorithm import Algorithm
@@ -15,20 +28,21 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def main():
-    # Load the current job details from the environment variables
-    job_details: JobDetails = OceanProtocolJobDetails().load()
+def main() -> None:
+    job_details = OceanProtocolJobDetails().load()
 
     logger.info("Starting compute job with the following input information:")
-    logger.info(
-        json.dumps(
-            {k: str(v) for k, v in asdict(job_details).items()},
-            sort_keys=True,
-            indent=4,
-        )
-    )
+    algorithm = Algorithm(job_details)
 
-    Algorithm(job_details).run().save_result(Paths.OUTPUTS / "profiling_report.html")
+    try:
+        algorithm.run()
+    except Exception as e:
+        logger.exception(f"An error occurred while running the algorithm: {e}")
+
+    try:
+        algorithm.save_result(Path(config.path_outputs) / "profiling_report.html")
+    except Exception as e:
+        logger.exception(f"An error occurred while saving the results: {e}")
 
 
 if __name__ == "__main__":
